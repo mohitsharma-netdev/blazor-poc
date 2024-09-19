7c4c90762b3c516703b8a5519e45f4707c595775
Mohit Sharma <mohitsharma.netdev@gmail.com>
2024-09-19 16:09:05 -0700

    Added a Blazor Server project to support middleware

---
+++ BlazorServerAppDemo/Program.cs
@@ -0,0 +1,42 @@
+using BlazorServerAppDemo.Components;
+using BlazorServerAppDemo.Services;
+
+namespace BlazorServerAppDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            // Register the TodoService
+            builder.Services.AddSingleton<TodoService>();
+
+            builder.Services.AddScoped(sp => new HttpClient ());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ BlazorServerAppDemo/Services/TodoService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorServerAppDemo.Models;
+
+namespace BlazorServerAppDemo.Services;
+
+public class TodoService
+{
+    private readonly List<TodoItem> _todos = new();
+
+    public IEnumerable<TodoItem> GetTodos() => _todos;
+
+    public void AddTodoItem(TodoItem item)
+    {
+        item.Id = _todos.Count + 1;
+        _todos.Add(item);
+    }
+
+    public void ToggleTodoItem(int id, bool isCompleted)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo != null)
+        {
+            todo.IsCompleted = isCompleted;
+        }
+    }
+
+    public void RemoveTodoItem(int id)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo != null)
+        {
+            _todos.Remove(todo);
+        }
+    }
+}
